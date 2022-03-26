58a75274be0340cd31411d281b7309f3802336c3
ian-otieno <ian.otieno@student.moringaschool.com>
2022-03-26 08:59:05 +0300

    add class location and images to models.py

--- gallery/models.py
+++ gallery/models.py
@@ -28,3 +28,32 @@ class Category(models.Model):
 
     def delete_category(self):
         self.delete()
+class Location(models.Model):
+    location_name = models.CharField(max_length=30)
+    
+
+    def __str__(self):
+        return self.location_name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+
+class Images(models.Model):
+    image_name = models.CharField(max_length=30)
+    image_description = models.TextField()
+    image = CloudinaryField('image')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.image_name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
