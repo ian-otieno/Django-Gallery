66c16a4c51b235c101a371c9047b0813726846fa
ian-otieno <ian.otieno@student.moringaschool.com>
2022-03-26 09:20:33 +0300

    add test class photographer and category to test.py

--- gallery/models.py
+++ gallery/models.py
@@ -1,8 +1,6 @@
-
 from django.db import models
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
 # Create your models here.
 class Photographer(models.Model):
     first_name = models.CharField(max_length=30)
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from .models import Photographer, Category, Location,Images
 
 # Create your tests here.
+class PhotographerTestClass(TestCase):
+    def setUp(self):
+        self.iano = Photographer(first_name = 'ian', last_name='owino', email='ianowino3@gmail.com')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.iano, Photographer))
+
+    def test_save_method(self):
+        self.iano.save_photographer()
+        photogarphers = Photographer.objects.all()
+        self.assertTrue(len(photogarphers)>0)
+
+    def test_delete_method(self):
+        self.iano.save_photographer()
+        photographers = Photographer.objects.all()
+        self.iano.delete_photographer()
+        self.assertTrue(len(photographers)==0)
+
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.new_category = Category(category_name="Food")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_category, Category))
+
+    def test_save_category(self):
+        self.new_category.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories)>0)
+
+    def test_delete_category(self):
+        self.new_category.save_category()
+        categories = Category.objects.all()
+        self.new_category.delete_category()
+        self.assertTrue(len(categories)==0)
