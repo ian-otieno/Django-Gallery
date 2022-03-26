26c5dbfd88aba89da1b6e745193809a7ba89ea34
ian-otieno <ian.otieno@student.moringaschool.com>
2022-03-26 10:47:10 +0300

    add test location and image function to test.py

--- gallery/tests.py
+++ gallery/tests.py
@@ -37,3 +37,31 @@ class CategoryTestClass(TestCase):
         categories = Category.objects.all()
         self.new_category.delete_category()
         self.assertTrue(len(categories)==0)
+
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.new_location = Location(location_name="Nairobi")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_location, Location))
+
+    def test_save_location(self):
+        self.new_location.save_location()
+        locations= Location.objects.all()
+        self.assertTrue(len(locations)>0)
+
+    def test_delete_category(self):
+        self.new_location.save_location()
+        locations= Location.objects.all()
+        self.new_location.delete_location()
+        self.assertTrue(len(locations)==0)
+
+class ImageTestCase(TestCase):
+    def setUp(self):
+        self.nairobi =Location(location_name='nairobi')
+        self.nairobi.save_location()
+        self.travel =Category(category_name='travel')
+        self.travel.save_category()
+
+        self.new_image = Images(image_name='leopard',image_description='lives in game park', location=self.nairobi, category=self.travel)
+        self.new_image.save()
