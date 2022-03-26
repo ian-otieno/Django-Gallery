fc50ccf87ec249029ef62333bd3070c99620f179
ian-otieno <ian.otieno@student.moringaschool.com>
2022-03-26 12:00:19 +0300

    add search,category and location function to views.py

--- gallery/views.py
+++ gallery/views.py
@@ -20,4 +20,29 @@ def image(request,image_id):
 
 def copy_to_clipboard(request):
     pass
+def search_results(request):
+    
+    if 'images' in request.GET and request.GET["images"]:
+        search_term = request.GET.get("images")
+        searched_images = Images.search_image_by_category(search_term)
+        message = f"{search_term}"
 
+        return render(request, 'all-gallery/search.html',{"message":message,"all_images": searched_images})
+
+    else:
+        message = "You haven't searched for any category"
+        return render(request, 'all-gallery/search.html',{"message":message})
+
+def get_category_images(request,category):
+    
+    image_categories = Images.get_images_by_category(category)
+    return render(request,'all-gallery/category.html',{'image-categories':image_categories})
+
+def get_images_by_location(request,location):
+   
+    location_images = Images.get_images_by_location(location)
+    return render(request,'all-gallery/location.html',{'location_images':location_images})
+
+
+
+    
