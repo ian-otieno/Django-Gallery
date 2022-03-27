bd858328803ac456e94b88b032e9ac9934341dd2
ian-otieno <ian.otieno@student.moringaschool.com>
2022-03-27 12:28:48 +0300

    add development comfig to settings.py

--- mygallery/settings.py
+++ mygallery/settings.py
@@ -19,12 +19,42 @@ import dj_database_url
 from decouple import config, Csv
 from pathlib import Path
 
+MODE=config("MODE",default="dev")
+SECRET_KEY=config('SECRET_KEY')
+DEBUG=config('DEBUG',default=False, cast=bool)
 
 cloudinary.config( 
   cloud_name = "iainoo", 
   api_key = "166495192544533", 
   api_secret = "1G7wj0mGvv5W8yw9JaOeFZkEUPE" 
 )
+
+#development
+if config('MODE')=="dev":
+    DATABASES={
+        'default':{
+            'ENGINE':  'django.db.backends.postgresql_psycopg2',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': '',
+        }
+    }
+
+#production
+else:
+    DATABASES={
+        'default': dj_database_url.config(
+            default=config('DATABASE_URL')
+        )
+    }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
 
 import os
 
